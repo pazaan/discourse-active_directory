d92c1b476d0a275234f9572926a4c9d954eafe5d
Goedhart, Lennart <LGoedhart@ea.com>
2015-10-05 21:41:41 +1100

    Changes to make discourse-active_directory work on latest version of Discourse
    Still can’t load the gems in the file, because pyu-ruby-sasl won’t
    install :(

--- plugin.rb
+++ plugin.rb
@@ -3,15 +3,14 @@
 # version: 0.1.0
 # author: Chris Wells <cwells@thegdl.org>
 
-gem 'omniauth-ldap', '1.0.4'
+require 'omniauth-ldap'
+#gem 'pyu-ruby-sasl', '0.0.3.1'
+#gem 'rubyntlm', '0.1.1'
+#gem 'net-ldap', '0.3.1'
+#gem 'omniauth-ldap', '1.0.4'
 
 class ADAuthenticator < ::Auth::Authenticator
 
-	DC = Discourse.PluginSettings[:active_directory].authad_domain_controller
-	BASE_DN = Discourse.PluginSettings[:active_directory].authad_base_dn
-	BIND_DN = Discourse.PluginSettings[:active_directory].authad_bind_dn
-	BIND_PASS = Discourse.PluginSettings[:active_directory].authad_bind_pass
-	
 	def name
 		'active_directory'
 	end
@@ -19,18 +18,25 @@ class ADAuthenticator < ::Auth::Authenticator
 	def after_authenticate(auth_token)
 		result = Auth::Result.new
 		
-		authad_uid = auth_token[:uid]
+		ad_uid = auth_token[:uid]
 		data = auth_token[:info]
 		result.email = email = data[:email]
-		result.name = name = data[:name]
+		result.name = name = data[:sAMAccountName]
+		result.email_valid = true
 
 		result.extra_data = {
-			uid: authad_uid,
+			uid: ad_uid,
 			provider: auth_token[:provider],
 			name: name,
 			email: email,
 		}
 		
+		user_info = User.find_by_email(email)
+
+		if user_info
+			result.user = user_info
+		end
+
 		result
 	end
 	
@@ -40,30 +46,37 @@ class ADAuthenticator < ::Auth::Authenticator
 	
 	def register_middleware(omniauth)
 		omniauth.provider :ldap,
-						  :host => DC,
-						  :port => 389,
-						  :method => :plain,
-						  :base => BASE_DN,
-						  :uid => 'sAMAccountName',
-						  :bind_dn => BIND_DN,
-						  :password => BIND_PASS
+						:title => "Active Directory Login",
+						:name => name,
+						:require => "omniauth-ldap",
+						:setup => lambda { |env|
+						  	strategy = env["omniauth.strategy"]
+						  	strategy.options[:host] = SiteSetting.ad_domain_controller
+						  	strategy.options[:port] = SiteSetting.ad_domain_controller_port
+							strategy.options[:base] = SiteSetting.ad_base_dn
+							strategy.options[:bind_dn] = SiteSetting.ad_bind_dn
+							strategy.options[:password] = SiteSetting.ad_bind_pass
+							strategy.options[:method] = :plain
+							strategy.options[:uid] = SiteSetting.ad_login_attribute
+							strategy.options[:name_proc] = Proc.new {|name| name.gsub(/@.*$/,'')}
+						}
 	end
 end
 
 auth_provider :title => 'with Active Directory',
 	:message => 'Log in with Active Directory',
-	:frame_width => 920,
-	:frame_height => 800,
+	:frame_width => 600,
+	:frame_height => 350,
 	:authenticator => ADAuthenticator.new
 	
 register_css <<CSS
 
-.btn-social.windows {
+.btn-social.active_directory {
 	background: #0052A4;
 }
 
-.btn-social.windows:before {
-	content: "N";
+.btn-social.active_directory:before {
+	content: "\\F17A";
 }
 
 CSS
